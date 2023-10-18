39040e89e6feb24eda5500d93fa2a989f2d8d124
pravalika1251 <146412054+pravalika1251@users.noreply.github.com>
2023-10-18 09:58:08 +0530

    Add files via upload

---
+++ Casting1.java
@@ -0,0 +1,11 @@
+class Cast1
+{
+  public static void main(String args[])
+  {
+   char c='a';
+   int x=c;
+   float f;
+   f=c;
+   System.out.println(c+" "+x+" "+f);
+}
+}
---
+++ Java.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Java
+{
+	public static void main(String args[])
+	{
+		System.out.println("HELLO WORLD");
+	}
+}
